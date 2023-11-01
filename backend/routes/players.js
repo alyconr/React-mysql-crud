fe84a69f2d8a0a38788af38fc0fa2d0f77bad4ca
alyconr <alyconr@hotmail.com>
2023-11-01 14:12:25 -0500

    :hammer: Configure route to get a single player

--- backend/routes/players.js
+++ backend/routes/players.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 
-const { getAllPlayers, addPlayer, deletePlayer, updatePlayer} = require('../controllers/players');
+const { getAllPlayers, getPlayer, addPlayer, deletePlayer, updatePlayer} = require('../controllers/players');
 
 
 router.route('/').get(getAllPlayers).post(addPlayer);
-router.route('/:id').delete(deletePlayer).put(updatePlayer);
+router.route('/:id').get(getPlayer).delete(deletePlayer).put(updatePlayer);
 
 
 
